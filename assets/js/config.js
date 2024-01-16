9fc907efddb0bb161828c43803fbf612031874e3
Eric <26416387.re@gmail.com>
2024-01-16 11:51:22 +0800

    first commit

---
+++ assets/js/config.js
@@ -0,0 +1,9 @@
+export const locationUrl='https://eric955.github.io/eric-outfitpals/';
+// export const locationUrl='http://localhost:5173/outfitpals/pages/';
+
+// export  const apiUrl='http://localhost:3000/';
+export const apiUrl='https://demo-zrr1.onrender.com/'; //render server
+
+
+export const localUrl='https://eric955.github.io/eric-outfitpals';
+// export const localUrl='http://localhost:5173/outfitpals/pages';
