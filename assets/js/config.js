16a96c87de4ffcb827a54176c6eb5aaf7ea8938e
Eric <26416387.re@gmail.com>
2024-01-16 18:55:18 +0800

    test: 💍 11111

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,9 +1,9 @@
-export const locationUrl='https://eric955.github.io/eric-outfitpals/';
+export const locationUrl='https://https://eric955.github.io/ericoutfitpals/';
 // export const locationUrl='http://localhost:5173/outfitpals/pages/';
 
 // export  const apiUrl='http://localhost:3000/';
 export const apiUrl='https://demo-zrr1.onrender.com/'; //render server
 
 
-export const localUrl='https://eric955.github.io/eric-outfitpals';
+export const localUrl='https://eric955.github.io/ericoutfitpals';
 // export const localUrl='http://localhost:5173/outfitpals/pages';
